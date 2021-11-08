524211aaa6257c8e1571780170d2a36e2992930c
karam1998 <karm.shaat@gmail.com>
2021-11-08 11:37:29 +0200

    sprint #1 _ui_ local all screens

---
+++ lib/screens/launch_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({Key? key}) : super(key: key);
+
+  @override
+  _LaunchScreenState createState() => _LaunchScreenState();
+}
+
+class _LaunchScreenState extends State<LaunchScreen> {
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home_screen');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
